76f1136fedde2d69943f5ba109ad3f34dceef209
denis <dublin87@mail.ru>
2023-04-02 23:19:41 +0300

    final

--- flashscore.py
+++ flashscore.py
@@ -9,10 +9,11 @@ from selenium.webdriver.common.keys import Keys
 import undetected_chromedriver 
 import os
 options = webdriver.ChromeOptions()
-driver=undetected_chromedriver.Chrome()
+from tkinter import *
+from tkinter import ttk
 import pandas as pd
 import glob
-
+import re
 
 from openpyxl import load_workbook
 from openpyxl.utils import get_column_letter, column_index_from_string
@@ -26,33 +27,52 @@ KomercialType=" "
 brendItem=" "
 typeItem=" "
 obiem=" "
-articul= ''
-width=''
+articul= " "
+width=" "
+
+hidth=" "
+anot=" "
+dlina=" "
+femal=" "
+ManufacturerСountry=" "
+linkUrl=" "
+count= " "
+Sostav= " "
+keyWord=[]
+linkUrldop=[]
+linkUrldop1=""
+aromaClass=" "
+keyWord1=" "
 #name,price,priceSkidka,nds,KomercialType
-def parser(driver,id):
-      
-    global name, price,priceSkidka,nds ,KomercialType,brendItem,typeItem,obiem,countItem,femal,classop,ManufacturerСountry,articul,width    
-    time.sleep(5)
+
+def parser(id):
+    driver=undetected_chromedriver.Chrome()
+    
+    global name, price,priceSkidka,nds,KomercialType,brendItem,typeItem,obiem,countItem,femal,classop,ManufacturerСountry,articul,width,linkUrl,hidth,anot,dlina,keyWord,linkUrldop,aromaClass,Sostav,keyWord1,linkUrldop1   
+    
+    time.sleep(1)
     # переход  на сайт  ozon
     html=driver.get("https://www.ozon.ru/")
-    time.sleep(5)
+    time.sleep(1)
     #ищем  поле поиска
     kcal = driver.find_element(By.XPATH,"//input[contains(@class,'tsBodyL')]")
-    time.sleep(5)
+    time.sleep(1)
     #вставляем  id
     kcal.send_keys(id)
-    time.sleep(5)
+    time.sleep(1)
     #лупа для продолжения поиска
-    lupa = driver.find_element(By.XPATH,'//button[@class="x5-a1 x5-f0"]')
-    time.sleep(5)
+    #lupa = driver.find_element(By.XPATH,'//button[@class="x5-a1 x5-f0"]')
+    #header-search-go
+    lupa = driver.find_element(By.XPATH,'//button[@type="submit"]')
+    time.sleep(1)
     lupa.click()
 
-    time.sleep(5)
+    time.sleep(1)
 
     # клик по ссылке  продукта 
     item=driver.find_element(By.XPATH,"//span[contains(@class,'tsBodyL')]")
     item.click()
-    time.sleep(5)
+    time.sleep(1)
     
 
     
@@ -60,7 +80,8 @@ def parser(driver,id):
     # ищем имя  товара
     
     try: 
-        name=driver.find_element(By.XPATH,"//h1[contains(@class,'rn')]").text
+        #name=driver.find_element(By.XPATH,"//h1[contains(@class,'rn')]").text
+        name= driver.find_element(By.XPATH,"//div[@data-widget='webProductHeading']").text
         name=name
         print(name)
     
@@ -70,7 +91,8 @@ def parser(driver,id):
     
     #ищем цену  товара
     try:
-        price=driver.find_element(By.XPATH,"//span[contains(@class,'p1n np2')]").text.replace("₽", "")
+        #price=driver.find_element(By.XPATH,"//span[contains(@class,'p1n np2')]").text.replace("₽", "")
+        price= driver.find_element(By.XPATH,"//div[@data-widget='webPrice']//child::span[1]").text.replace("₽", "")
         price=price
         print(price)
         
@@ -80,7 +102,8 @@ def parser(driver,id):
     
     #ищем цену  до скидки товара
     try:
-        priceSkidka=driver.find_element(By.XPATH,"//span[contains(@class,'pn2')]").text.replace("₽", "") 
+        #priceSkidka=driver.find_element(By.XPATH,"//span[contains(@class,'pn2')]").text.replace("₽", "")
+        priceSkidka=driver.find_element(By.XPATH,"//div[@data-widget='webPrice']//child::span[2]").text.replace("₽", "") 
         priceSkidka=priceSkidka 
         print(priceSkidka)      
     except:
@@ -104,34 +127,65 @@ def parser(driver,id):
     
     #Ширина
     try:
-        #r=re.match(\d[0-9])
-        width=driver.find_element(By.XPATH,"//span[text()='Размер упаковки']/following::dd[1]").text
-        width=width
-        print(width)
+        
+        width=driver.find_element(By.XPATH,"//span[text()='Размер упаковки (Длина х Ширина х Высота), см']/following::dd[1]").text
+        #r=re.search('[0-9]',width)
+        r=re.search(r"\d{1,3}",width)
+        width=r[0]
+        print(r[0])
     except:
         width=''
         print('элемент width не найден')
     #высота
     try:
-        #r=re.match(\d[0-9])
-        hidth=driver.find_element(By.XPATH,"//span[text()='Размер упаковки']/following::dd[1]").text
-        hidth=hidth
-        print(hidth)
+        #r=re.match(r'\d[0-9]')
+        hidth=driver.find_element(By.XPATH,"//span[text()='Размер упаковки (Длина х Ширина х Высота), см']/following::dd[1]").text
+        r2=re.search(r"[*]\d{1,3}",hidth)
+        hidth=r2[0].replace("*", "")
+        print(r2[0].replace("*", ""))
     except:
         hidth=''
         print('элемент hidth не найден')
     #Длина
     try:
-        #r=re.match(\d[0-9])
-        width=driver.find_element(By.XPATH,"//span[text()='Размер упаковки']/following::dd[1]").text
-        width=width
-        print(width)
+        dlina=driver.find_element(By.XPATH,"//span[text()='Размер упаковки (Длина х Ширина х Высота), см']/following::dd[1]").text
+        r3=re.search(r"\d{1,3}$",dlina)
+        dlina=r3[0]
+        print(r3[0])
     except:
         width=''
         print('элемент width не найден')
 
+    #url ссылка на картинку
+    try:
 
-   
+        linkUrl=driver.find_element(By.XPATH,'//img[@fetchpriority="high"]').get_attribute("src")
+
+        
+        linkUrl=linkUrl
+        print(linkUrl)
+    except:
+        linkUrl=''
+        print('элемент LinkURL не найден')
+ 
+ 
+    #url ссылка на доп картинку
+    try:
+        
+        linkUrldop.append(driver.find_element(By.XPATH,'//div[@data-index="1"]/child::div/child::img').get_attribute("src"))
+        linkUrldop.append(driver.find_element(By.XPATH,'//div[@data-index="2"]/child::div/child::img').get_attribute("src"))
+        linkUrldop.append(driver.find_element(By.XPATH,'//div[@data-index="3"]/child::div/child::img').get_attribute("src"))
+        linkUrldop.append(driver.find_element(By.XPATH,'//div[@data-index="4"]/child::div/child::img').get_attribute("src"))
+
+        linkUrldop1=",".join(linkUrldop)
+        linkUrldop1=linkUrldop1.replace(",",";")
+        
+        print(linkUrldop1)   
+        
+    except:
+        linkUrldop=''
+        print('элемент linkUrldop не найден')
+
     #SerialNumber
     #SerialNumber=driver.find_element(By.XPATH,"//span[text()='Тип']/following::dd[1]")
     #print(SerialNumber.text)
@@ -139,11 +193,7 @@ def parser(driver,id):
     Ozonid=id
     print(Ozonid)
     #ищем тип  товара 
-    #
-
-
-    #ссылка на главное фото 
-    #mainFoto=driver.find_element(By.XPATH,"//input[contains(@class,'tsBodyL')]")")
+    
     #бренд   товара
     try:
         brendItem=driver.find_element(By.XPATH,"//span[text()='Бренд']/following::dd[1]").text
@@ -160,6 +210,7 @@ def parser(driver,id):
         typeItem=driver.find_element(By.XPATH,"//span[text()='Тип']/following::dd[1]").text
         typeItem=typeItem
         print(typeItem)
+        type(typeItem)
     except:
         typeItem=''
         print('элемент typeItem не найден')
@@ -176,7 +227,7 @@ def parser(driver,id):
     countItem=countItem
     print(countItem)  
     
-    #объем товара
+    #артикул товара
     try:
         articul=driver.find_element(By.XPATH,"//span[text()='Артикул']/following::dd[1]").text
         articul=articul
@@ -198,11 +249,19 @@ def parser(driver,id):
     except:
         femal=''  
         print('Элемент femal не найден')
-    #класс опасности
-    classop="не опасен"
-    classop=classop
-    print(classop)
-    
+
+
+    #аннотации
+    try:
+        #anot=driver.find_element_by_id("section-description")/child::div[4]")
+        anot=driver.find_element(By.XPATH,"//h2[text()='Описание']/following::div[1]").text
+
+        anot=anot
+        print(anot)
+    except:
+        anot=''  
+        print('Элемент anot не найден')
+
 
     #страна изготовитель
     try:
@@ -212,46 +271,148 @@ def parser(driver,id):
     except:
         ManufacturerСountry=''
         print('элемент ManufactureCountry не найден')    
-       
+     
+
+
+    #классификация аромата
+    try:
+        aromaClass=driver.find_element(By.XPATH,"//span[text()='Классификация аромата']/following::dd[1]").text
+        aromaClass=aromaClass
+        print(aromaClass)
+    except:
+        aromaClass=''
+        print('элемент aromaClass  не найден') 
+    #состав
+    try:
+        Sostav=driver.find_element(By.XPATH,"//*[text()='Состав']/following-sibling::p").text
+        Sostav=Sostav
+        Sostav=Sostav.replace(", ",";")
+        
+        print(Sostav)
+    except:
+        Sostav=''
+        print('элемент Sostav  не найден')    
+   
+   #ключевые  слова
+    
+    try:
+        #,aromaClass
+        keyWord=[brendItem,aromaClass,Sostav,typeItem]
+        
+        keyWord1=','.join(keyWord)
+
+        keyWord1=keyWord1.replace(",", ";")
+
+        #keyWord1=keyWord1.replace("; ;", "")   
+        print(keyWord1)
+    except:
+        keyWord1=''
+        print("элемент keyWord не найден")
+
+
+    driver.close()  
+    driver.quit()
+
     return id,name,price,priceSkidka,nds,KomercialType,articul
  
  
-
+    
 
 #,name,price,priceSkidka,nds,KomercialType
-def OpenPixx(file,id,r,articul,name,price,priceSkidka,nds,KomercialType):
+#articul,name,price,priceSkidka,nds,KomercialType,obiem,width,higth,dlina,linurl,linkurldrop,brendItem,femal,anot,Sostav,CoutItem
+def OpenPixx(file,id,r):
     hel='hello'
     wb = load_workbook(file)
     # get_sheet_names() - выводит список с названием листов,
     ws = wb['Шаблон для поставщика']
     #ws['B4']=id
     
-    count=1
+    #i=1
     ws.cell(row=r, column=1).value = count  #номер  столбца
     ws.cell(row=r, column=2).value = articul    #артикул
     ws.cell(row=r, column=3).value = name   #название товара
     ws.cell(row=r, column=4).value = price  #цена товара
     ws.cell(row=r, column=5).value = priceSkidka # цена до скидки
-    ws.cell(row=r, column=6).value = nds
-    ws.cell(row=r, column=7).value = id     #id
+    ws.cell(row=r, column=6).value = None #nds
+    ws.cell(row=r, column=7).value = id     #id 7 поле
     ws.cell(row=r, column=8).value = KomercialType
     ws.cell(row=r, column=9).value = None
-    ws.cell(row=r, column=10).value = obiem
-    ws.cell(row=r, column=11).value = KomercialType #вес упаковки
-    ws.cell(row=r, column=12).value = KomercialType #ширина упаковки
-    ws.cell(row=r, column=12).value = KomercialType #высота упаковки
+    ws.cell(row=r, column=10).value = obiem   #вес в упаковке в г 10 поле
+    ws.cell(row=r, column=11).value = width #ширина упаковки
+    ws.cell(row=r, column=12).value = hidth #высота  упаковки
+    ws.cell(row=r, column=13).value = dlina #длина упаковки 13 поле
+    ws.cell(row=r, column=14).value = linkUrl #cсылка на главное фото
+    ws.cell(row=r, column=15).value = linkUrldop1 #ссылка на доп фото      
+    ws.cell(row=r, column=16).value = None #ссылка на фото 360
+    ws.cell(row=r, column=17).value = None #артикул фото
+    ws.cell(row=r, column=18).value = brendItem #бренд 18 поле
+    ws.cell(row=r, column=19).value = typeItem #тип
+    ws.cell(row=r, column=20).value = None #Объединить на одной карточке
+    ws.cell(row=r, column=21).value = obiem #Объем, мл*
+    ws.cell(row=r, column=22).value = countItem #Единиц в одном товаре
+    ws.cell(row=r, column=23).value = femal #пол
+    ws.cell(row=r, column=24).value = None #Срок годности в днях
+    ws.cell(row=r, column=25).value = None #Класс опасности товара
+    ws.cell(row=r, column=26).value = None #Rich-контент JSON
+    ws.cell(row=r, column=27).value = None #Минимальный возраст ребенка
+    ws.cell(row=r, column=28).value = None #Максимальный возраст ребенка
+    ws.cell(row=r, column=29).value = None #Планирую доставлять товар в нескольких упаковках
+    ws.cell(row=r, column=30).value = anot #Аннотация
+    
+    ws.cell(row=r, column=31).value = keyWord1 #Ключевые слова
+    ws.cell(row=r, column=32).value = None #Серии
+    ws.cell(row=r, column=33).value = aromaClass #Классификация аромата
+    ws.cell(row=r, column=34).value = ManufacturerСountry #Страна-изготовитель
+    ws.cell(row=r, column=35).value = obiem #Вес, г
+    ws.cell(row=r, column=36).value = None #Упаковка
+    ws.cell(row=r, column=37).value = None #Целевая аудитория
+    ws.cell(row=r, column=38).value = Sostav #Состав
+    ws.cell(row=r, column=39).value = None #Особенности состава
+    ws.cell(row=r, column=40).value = None #Вид парфюмерии (служебный)
+    ws.cell(row=r, column=41).value = None #Код ОКПД/ТН ВЭД для парфюмерии
+    ws.cell(row=r, column=42).value = None #Количество заводских упаковок
+    ws.cell(row=r, column=43).value = None #Ошибка
+    ws.cell(row=r, column=44).value = None #Предупреждение
+    
     wb.save(file)
     return 1
 
-id=215973958    
-parser(driver,id)
-print(name)
-print(ManufacturerСountry)
 
+def  get_str_number(file):
+    wb = load_workbook(file)
+    ws = wb['Шаблон для поставщика']
+    i=4
+    count=1 
+    if (ws.cell(row=i,column=1).value) ==None:
+        r=4
+        count=1
+    else:
+        for i  in  range(1,1000):
+            if (ws.cell(row=i,column=1).value) !=None:
+                r=ws.cell(row=i,column=1).row
+                #count=ws.cell(row=i,column=1).value
+                r+=1
+                
+                
+    #print(r)
+    wb.close()
+    return r           
+
+def get_count(r):
+    global count
+    count=r-3                
+    #print(count)
+    
+    return count           
+ 
+id=154369096
+'''
+parser(id)
+'''
+'''
 file ="C:\\ozon1\\Парфюмерия.xlsx"
-#print(name,price,priceSkidka,nds,KomercialType)
-#openExel(file)
-r=9
-c=3
-OpenPixx(file,id,r,articul,name,price,priceSkidka,nds,KomercialType)
+r=get_str_number(file)
+count=get_count(r)
+OpenPixx(file,id,r)
 
+'''
--- visual.py
+++ visual.py
@@ -1,27 +1,76 @@
 from tkinter import *
 from tkinter import ttk
-import re
+#import re
+#import os
+#import undetected_chromedriver 
+from flashscore import parser,get_str_number,get_count,OpenPixx
+import pyperclip
 '''
-def show_message():
-    label["text"] = e1.get()     # получаем введенный текст
 
-def is_valid(newval):
-    count=8
-    if count!=8
-    return re.match("^\d[0-9]$", newval) is not None
-    '''
+    215973958
+   154369096
+   646065674
+
+
+
+829002092
+724581115
+825952201
+154369096
+215973958
+
+
+875752810
+     '''
+def bufer(event):
+    q = pyperclip.paste()
+    e1.insert(0, q)
+    return e1
+    
+    return 1 
+def func_parce(event):
+    #while e1.get()==None:
+        #print('введите id')
+    id=e1.get()
+    parser(id)
+    file ="C:\\ozon1\\Парфюмерия.xlsx"
+    r=get_str_number(file)
+    count=get_count(r)
+    OpenPixx(file,id,r)
+    #print('Hello')
+    return 1
 #окно класс Tk
 root = Tk() 
 root.title("Парсер")
 #размер окна
 root.geometry("400x450")
+
+#lbl = Label(root, text="Введите id")
+label = ttk.Label(root, text="Введите id (9 цифр)")
+label.pack(fill=X, padx=[135, 10], pady=20)
 e1 = Entry(width=50)
-#e1.show()
-e1.pack()
-btn = ttk.Button(text="начать парсинг" )
-label = ttk.Label()
-label.pack(anchor=NW, padx=6, pady=6)
+e1.pack(fill=X, padx=[60, 60], pady=30)
+
+btn1 = ttk.Button(root,  text="вставить скопированный текст",width=40)
+
+
+btn = ttk.Button(root,  text="начать парсинг",width=20)
+
+'''
+label1 = ttk.Label(root, text="вывод данных")
+label1.pack(fill=X, padx=[155, 10], pady=10)
+text = Text(width=25, height=5,
+            fg='white', wrap=WORD)
+ 
+text.pack()
+'''
+#label.pack(anchor=NW, padx=6, pady=6)
+btn1.pack()
 btn.pack(expand = 1)
+
+btn1.bind("<Button-1>", bufer)
+btn.bind("<Button-1>", func_parce)
+
 #show_message()
 root.mainloop()
 
